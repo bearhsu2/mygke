1b9aef20003c4a774484e0726238bcf524164194
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-05 22:07:23 +0800

    add build scripts

---
+++ efk_practice/build.sh
@@ -0,0 +1,1 @@
+docker build -t bearhsu2/kuma-efk:1.0 .
--- efk_practice/run.sh
+++ efk_practice/run.sh
@@ -1,6 +1,9 @@
 CONTAINER_NAME=KumaEFK
 
+WORKING_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
 docker rm -f $CONTAINER_NAME
 docker run --name $CONTAINER_NAME  -d \
   -p 5601:5601 -p 24224:24224 -p 9200:9200 -p 24220:24220 \
-  lis0x90/efk
+  bearhsu2/kuma-efk:1.0
